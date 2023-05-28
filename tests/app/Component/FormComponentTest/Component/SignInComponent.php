e72f0d7009420f98edccb0bc915d0c5f5e4d859e
Maxim Smakouz <m.smakouz@gmail.com>
2023-05-28 14:42:42 +0300

    Fix tests

--- tests/app/Component/FormComponentTest/Component/SignInComponent.php
+++ tests/app/Component/FormComponentTest/Component/SignInComponent.php
@@ -6,11 +6,17 @@ namespace Spiral\Symfony\Form\Tests\App\Component\FormComponentTest\Component;
 
 use Spiral\Symfony\Form\Component\FormComponent;
 use Spiral\Symfony\Form\Tests\App\Component\FormComponentTest\Form\SignInForm;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 
 final class SignInComponent extends FormComponent
 {
     private array $data = [];
+
+    public function __construct(
+        private readonly FormFactoryInterface $formFactory
+    ) {
+    }
 
     public function createForm(): FormInterface
     {
